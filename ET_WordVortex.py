9add9ecf138b3fc3accf01eeec6a47ce5993e099
CreeperMaster88 <etroop719@gmail.com>
2019-12-11 12:39:45 -0500

    VICTORY!!!!!!!!!!!

--- ET_WordVortex.py
+++ ET_WordVortex.py
@@ -1,10 +1,19 @@
-word = raw_input("Enter a word")
-vortex = len(word + word[-2:-len(word)-1:-1])
-wordv=word + word[-2:-len(word)-1:-1]
-wordlist = []
-for i in wordv:
-    wordlist.append(i)
-for i in range(vortex):
-    for j in range(vortex):
-        print wordlist[i],
-    print
+def vortex(word):
+    wordVortex = []
+    Length = len(word) * 2 - 1
+    for row in range(Length):
+        wordVortex.append([])
+        for column in range(Length):
+            wordVortex[row].append(word[min(row, column, Length - row - 1, Length - column - 1)])
+
+    for i in range(len(wordVortex)):
+        for j in range(len(wordVortex[i])):
+            print wordVortex[i][j],
+        print
+
+
+def Main():
+    vortex(raw_input("Enter a word or phrase."))
+
+
+Main()
