6974a47b18a8fc52e5ade312e728c6868326d412
CreeperMaster88 <etroop719@gmail.com>
2020-01-05 20:42:09 -0500

    boring lol took 2 minutes

---
+++ actualAssignmentBalls.py
@@ -0,0 +1,17 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode([640, 480])
+screen.fill([255, 255, 255])
+pygame.draw.circle(screen, [255, 0, 0], [260, 200], 10, 0)
+pygame.draw.circle(screen, [255, 255, 0], [380, 210], 15, 0)
+pygame.draw.circle(screen, [0, 255, 0], [440, 220], 20, 0)
+pygame.draw.circle(screen, [0, 255, 255], [200, 230], 25, 0)
+pygame.draw.circle(screen, [0, 0, 255], [320, 240], 30, 0)
+pygame.display.flip()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
